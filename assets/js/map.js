075a79f023f57586a3cf8fb9f07bb9b6e2fa0df5
FIRST_NAME LAST_NAME <50301659+Narrrsss@users.noreply.github.com>
2020-06-21 15:28:03 +0400

    deleted

--- assets/js/map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {} // now it IS a function and it is in global
-
-$(() => {
-  initMap = function() {
-    // your code like...
-    var map = new google.maps.Map(document.getElementById('map'), {/*your code*/});
-    // and other stuff...
-  }
-})
